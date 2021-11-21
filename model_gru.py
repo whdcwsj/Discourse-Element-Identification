94157b968555655b5ece7ec4ccb3ee9fb17bef08
whdcwsj <maxinaerichared@gmail.com>
2021-11-21 10:54:30 +0800

    添加model_gru.py

--- model_gru.py
+++ model_gru.py
@@ -5,103 +5,11 @@ import torch.nn.functional as F
 from subLayer import *
 
 
-# 无句子间特征模型
-# 少了pool_type
-# 只添加了句子位置编码与过了LST妈的document进行加和，没有使用两个句间注意力
-class STModel(nn.Module):
-    def __init__(self, word_dim, hidden_dim, sent_dim, class_n, p_embd=None, pos_dim=0, p_embd_dim=16):
-        # p_embd: 'cat', 'add','embd', 'embd_a'
-        super(STModel, self).__init__()
-        self.word_dim = word_dim
-        self.hidden_dim = hidden_dim
-        self.sent_dim = sent_dim
-        self.class_n = class_n
-        self.p_embd = p_embd
-        self.p_embd_dim = p_embd_dim
-
-        # self.dropout = nn.Dropout(p=0.1)
-
-        self.sentLayer = nn.LSTM(self.word_dim, self.hidden_dim, bidirectional=True)
-        self.classifier = nn.Linear(self.sent_dim * 2, self.class_n)
-        # self.classifier2 = nn.Linear(self.sent_dim, self.class_n)
-
-        self.posLayer = PositionLayer(p_embd, p_embd_dim)
-
-        if p_embd == 'embd':
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2 + p_embd_dim * 3, self.sent_dim, bidirectional=True)
-        elif p_embd == 'cat':
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2 + 3, self.sent_dim, bidirectional=True)
-        else:
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2, self.sent_dim, bidirectional=True)
-
-    def init_hidden(self, batch_n, doc_l, device='cpu'):
-        self.sent_hidden = (torch.rand(2, batch_n * doc_l, self.hidden_dim, device=device).uniform_(-0.01, 0.01),
-                            torch.rand(2, batch_n * doc_l, self.hidden_dim, device=device).uniform_(-0.01, 0.01))
-        self.tag_hidden = (torch.rand(2, batch_n, self.sent_dim, device=device).uniform_(-0.01, 0.01),
-                           torch.rand(2, batch_n, self.sent_dim, device=device).uniform_(-0.01, 0.01))
-
-    def forward(self, documents, pos=None, device='cpu', mask=None):
-        batch_n, doc_l, sen_l, _ = documents.size()  # documents: (batch_n, doc_l, sen_l, word_dim)
-        self.init_hidden(batch_n=batch_n, doc_l=doc_l, device=device)
-        documents = documents.view(batch_n * doc_l, sen_l, -1).transpose(0,
-                                                                         1)  # documents: (sen_l, batch_n*doc_l, word_dim)
-
-        sent_out, _ = self.sentLayer(documents, self.sent_hidden)  # sent_out: (sen_l, batch_n*doc_l, hidden_dim*2)
-
-        if mask is None:
-            sentpres = torch.tanh(torch.mean(sent_out, dim=0))  # sentpres: (batch_n*doc_l, hidden_dim*2)
-        else:
-            sent_out = sent_out.masked_fill(mask.transpose(1, 0).unsqueeze(-1).expand_as(sent_out), 0)
-            sentpres = torch.tanh(torch.sum(sent_out, dim=0) / (sen_l - mask.sum(dim=1).float() + 1e-9).unsqueeze(-1))
-
-        # sentpres = torch.tanh(sent_out[-1])     # sentpres: (batch_n*doc_l, hidden_dim*2)
-
-        sentpres = sentpres.view(batch_n, doc_l, self.hidden_dim * 2)  # sentpres: (batch_n, doc_l, hidden_dim*2)
-        # sentpres = self.dropout(sentpres)
-        # self.sent1 = sentpres   
-
-        sentpres = self.posLayer(sentpres, pos)
-        # self.sent2 = sentpres
-
-        sentpres = sentpres.transpose(0, 1)
-
-        tag_out, _ = self.tagLayer(sentpres, self.tag_hidden)  # tag_out: (tag_out, batch_n, output_dim*2)
-        tag_out = torch.tanh(tag_out)
-        # self.sent3 = tag_out
-
-        tag_out = tag_out.transpose(0, 1)  # tag_out: (batch_n, doc_l, sent_dim*2)
-
-        result = self.classifier(tag_out)
-
-        result = F.log_softmax(result, dim=2)  # result: (batch_n, doc_l, class_n)
-        return result
-
-    def getModelName(self):
-        name = 'st'
-        name += '_' + str(self.hidden_dim) + '_' + str(self.sent_dim)
-        if self.p_embd == 'cat':
-            name += '_cp'
-        elif self.p_embd == 'add':
-            name += '_ap'
-        elif self.p_embd == 'embd':
-            name += '_em'
-        elif self.p_embd == 'embd_a':
-            name += '_em_a'
-        elif self.p_embd:
-            name += '_' + self.p_embd
-        return name
-
-
-# type_id=0
-# 用于中文数据集的情况下：
-# 输入：vector_size=200，hidden_size=128，sent_dim=128，class_n=8，p_embd='add'，p_embd_dim=16，pool_type='max_pool'
-# 英文数据集：
-# p_embd = None
-class STWithRSbySPP(nn.Module):
+class STWithRSbySPP_GRU(nn.Module):
     def __init__(self, word_dim, hidden_dim, sent_dim, class_n, p_embd=None, pos_dim=0, p_embd_dim=16,
                  pool_type='max_pool'):
         # p_embd: 'cat', 'add','embd', 'embd_a'
-        super(STWithRSbySPP, self).__init__()
+        super(STWithRSbySPP_GRU, self).__init__()
         self.word_dim = word_dim
         self.hidden_dim = hidden_dim
         self.sent_dim = sent_dim
@@ -111,8 +19,8 @@ class STWithRSbySPP(nn.Module):
         self.pool_type = pool_type
 
         self.dropout = nn.Dropout(0.1)
-        self.sentLayer = nn.LSTM(self.word_dim, self.hidden_dim, bidirectional=True)
-        # 为什么sent_dim*2+30？？？因为要接两个句间注意力
+        self.sentLayer = nn.GRU(self.word_dim, self.hidden_dim, bidirectional=True)
+
         self.classifier = nn.Linear(self.sent_dim * 2 + 30, self.class_n)
         # self.classifier = nn.Linear(self.sent_dim * 2 + 60, self.class_n)
 
@@ -124,14 +32,12 @@ class STWithRSbySPP(nn.Module):
         # self.sfLayer = InterSentenceSPPLayer3(self.hidden_dim*2, pool_type = self.pool_type)
         # self.rfLayer = InterSentenceSPPLayer3(self.hidden_dim*2, pool_type = self.pool_type)
 
-        # 这个是将三个位置编码torch.cat到了一起，所以input_size要再加上 p_embd_dim*3
         if p_embd == 'embd':
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2 + p_embd_dim * 3, self.sent_dim, bidirectional=True)
-        # 这个是将三个位置编码torch.cat到了一起，所以input_size要再加上3
+            self.tagLayer = nn.GRU(self.hidden_dim * 2 + p_embd_dim * 3, self.sent_dim, bidirectional=True)
         elif p_embd == 'cat':
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2 + 3, self.sent_dim, bidirectional=True)
+            self.tagLayer = nn.GRU(self.hidden_dim * 2 + 3, self.sent_dim, bidirectional=True)
         else:
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2, self.sent_dim, bidirectional=True)
+            self.tagLayer = nn.GRU(self.hidden_dim * 2, self.sent_dim, bidirectional=True)
 
     # batch_size，一篇文章的句子个数
     def init_hidden(self, batch_n, doc_l, device='cpu'):
@@ -155,20 +61,12 @@ class STWithRSbySPP(nn.Module):
         self.init_hidden(batch_n=batch_n, doc_l=doc_l, device=device)
         documents = documents.view(batch_n * doc_l, sen_l, -1).transpose(0,1)  # documents: (sen_l, batch_n*doc_l, word_dim)
 
-        # 双向nn.LSTM()
-        # input：(40,batch_n*doc_l,200) → (seq_len, batch, input_size)
-        # (h_0,c_0)：h_0是隐藏层的初始状态，c_0是初始化的细胞状态
-        # 这两者shape均为: (num_layers * num_directions, batch, hidden_size)
-        # 输出：output,(h_n,c_n)
-        # output保存RNN最后一层的输出的Tensor，(seq_len, batch, hidden_size * num_directions) → (40,batch_n*doc_l,hidden_dim*2)
-        # (h_n，c_n):保存着RNN最后一个时间步的隐藏层状态；保存着RNN最后一个时间步的细胞状态
-        # shape：(num_layers * num_directions, batch, hidden_size)
+
         sent_out, _ = self.sentLayer(documents, self.sent_hidden)  # sent_out: (sen_l, batch_n*doc_l, hidden_dim*2)
         # sent_out = self.dropout(sent_out)
 
         if mask is None:
             # sentpres：(batch_n*doc_l,1,256)
-            # 激活函数
             sentpres = torch.tanh(torch.mean(sent_out, dim=0))  # sentpres: (batch_n*doc_l, hidden_dim*2)
         else:
             sent_out = sent_out.masked_fill(mask.transpose(1, 0).unsqueeze(-1).expand_as(sent_out), 0)
@@ -178,16 +76,11 @@ class STWithRSbySPP(nn.Module):
 
         # sentence embedding的句间注意力
         sentFt = self.sfLayer(sentpres)  # sentFt:(batch_n, doc_l,15)
-        sentFt = self.dropout(sentFt)
+        # sentFt = self.dropout(sentFt)
 
-        # "add"情况下，将前三个pos位置1：1：1与sentence加和; ['gpos', 'lpos', 'ppos']
         sentpres = self.posLayer(sentpres, pos)  # sentpres:(batch_n, doc_l, hidden_dim*2)
         sentpres = sentpres.transpose(0, 1)  # sentpres: (doc_l, batch_n, hidden_dim*2)
 
-        # LSTM(self.hidden_dim*2, self.sent_dim, bidirectional=True)
-        # input：(seq_len, batch, input_size)，input_size应该=LSTM的第一个参数，也就是self.hidden_dim*2
-        # (h_0,c_0)：(num_layers * num_directions, batch, hidden_size)
-        # output: (seq_len, batch, sent_dim * num_directions)
         tag_out, _ = self.tagLayer(sentpres, self.tag_hidden)  # tag_out: (doc_l, batch_n, sent_dim*2)
         # tag_out = self.dropout(tag_out)
 
@@ -195,21 +88,19 @@ class STWithRSbySPP(nn.Module):
 
         tag_out = tag_out.transpose(0, 1)  # tag_out: (batch_n, doc_l, sent_dim*2)
         roleFt = self.rfLayer(tag_out)  # roleFt:(batch_n, doc_l, 15)
-        roleFt = self.dropout(roleFt)
+        # roleFt = self.dropout(roleFt)
 
         tag_out = torch.cat((tag_out, sentFt, roleFt), dim=2)  # tag_out: (batch_n, doc_l, sent_dim*2+30)  (1,8,286)
         # tag_out = self.dropout(tag_out)
 
-        # class_n用在了这里
         result = self.classifier(tag_out)  # tag_out: (batch_n, doc_l, class_n)
 
-        # log_softmax 和 nll_loss配套使用
         result = F.log_softmax(result, dim=2)  # result: (batch_n, doc_l, class_n)
         return result
 
     def getModelName(self):
         # pool_type='max_pool'的第一个字母m
-        name = 'st_rs_spp%s' % self.pool_type[0]
+        name = 'st_rs_gru_spp%s' % self.pool_type[0]
         name += '_' + str(self.hidden_dim) + '_' + str(self.sent_dim)
         if self.p_embd == 'cat':
             name += '_cp'
@@ -225,12 +116,12 @@ class STWithRSbySPP(nn.Module):
 
 
 
-# type_id=1
-class EnSTWithRSbySPP(nn.Module):
+# 将基本的LSTM模型替换为GRU
+class EnSTWithRSbySPP_GRU(nn.Module):
     def __init__(self, word_dim, hidden_dim, sent_dim, class_n, p_embd=None, pos_dim=0, p_embd_dim=16,
                  pool_type='max_pool'):
         # p_embd: 'cat', 'add','embd', 'embd_a'
-        super(EnSTWithRSbySPP, self).__init__()
+        super(EnSTWithRSbySPP_GRU, self).__init__()
         self.word_dim = word_dim
         self.hidden_dim = hidden_dim
         self.sent_dim = sent_dim
@@ -239,11 +130,10 @@ class EnSTWithRSbySPP(nn.Module):
         self.p_embd_dim = p_embd_dim
         self.pool_type = pool_type
 
-        self.dropout = nn.Dropout(0.5)
-        self.sentLayer = nn.LSTM(self.word_dim, self.hidden_dim, bidirectional=True)
-        # 为什么sent_dim*2+30？？？因为要接两个句间注意力
+        self.dropout = nn.Dropout(0.1)
+        self.sentLayer = nn.GRU(self.word_dim, self.hidden_dim, bidirectional=True)
+
         self.classifier = nn.Linear(self.sent_dim * 2 + 30, self.class_n)
-        # self.classifier = nn.Linear(self.sent_dim * 2 + 60, self.class_n)
 
         self.posLayer = PositionLayer(p_embd, p_embd_dim)
 
@@ -254,24 +144,17 @@ class EnSTWithRSbySPP(nn.Module):
         # self.rfLayer = InterSentenceSPPLayer3(self.hidden_dim*2, pool_type = self.pool_type)
 
         if p_embd == 'embd':
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2 + p_embd_dim * 3, self.sent_dim, bidirectional=True)
+            self.tagLayer = nn.GRU(self.hidden_dim * 2 + p_embd_dim * 3, self.sent_dim, bidirectional=True)
         elif p_embd == 'cat':
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2 + 3, self.sent_dim, bidirectional=True)
+            self.tagLayer = nn.GRU(self.hidden_dim * 2 + 3, self.sent_dim, bidirectional=True)
         else:
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2, self.sent_dim, bidirectional=True)
+            self.tagLayer = nn.GRU(self.hidden_dim * 2, self.sent_dim, bidirectional=True)
 
     # batch_size，一篇文章的句子个数
     def init_hidden(self, batch_n, doc_l, device='cpu'):
-        # self.sent_hidden作为nn.LSTM()的第二个输入
-        # 两个元组，shape都是(2,batch_n*doc_l,hidden_dim)
-        # batch_n*doc_l相当于句子数量，
-        # torch.rand()均匀分布，从区间[0, 1)的均匀分布中抽取的一组随机数
-        # uniform_(),将tensor从均匀分布中抽样数值进行填充
-        self.sent_hidden = (torch.rand(2, batch_n * doc_l, self.hidden_dim, device=device).uniform_(-0.01, 0.01),
-                            torch.rand(2, batch_n * doc_l, self.hidden_dim, device=device).uniform_(-0.01, 0.01))
-        # 两个元组，shape都是(2,batch_n,hidden_dim)
-        self.tag_hidden = (torch.rand(2, batch_n, self.sent_dim, device=device).uniform_(-0.01, 0.01),
-                           torch.rand(2, batch_n, self.sent_dim, device=device).uniform_(-0.01, 0.01))
+        # h0: [num_layers * num_directions, batch, hidden_size]
+        self.sent_hidden = torch.rand(2, batch_n * doc_l, self.hidden_dim, device=device).uniform_(-0.01, 0.01)
+        self.tag_hidden = torch.rand(2, batch_n, self.sent_dim, device=device).uniform_(-0.01, 0.01)
 
     # 测试集情况
     # document:(batch_n,doc_l,40,200)
@@ -282,20 +165,10 @@ class EnSTWithRSbySPP(nn.Module):
         self.init_hidden(batch_n=batch_n, doc_l=doc_l, device=device)
         documents = documents.view(batch_n * doc_l, sen_l, -1).transpose(0,1)  # documents: (sen_l, batch_n*doc_l, word_dim)
 
-        # 双向nn.LSTM()
-        # input：(40,batch_n*doc_l,200) → (seq_len, batch, input_size)
-        # (h_0,c_0)：h_0是隐藏层的初始状态，c_0是初始化的细胞状态
-        # 这两者shape均为: (num_layers * num_directions, batch, hidden_size)
-        # 输出：output,(h_n,c_n)
-        # output保存RNN最后一层的输出的Tensor，(seq_len, batch, hidden_size * num_directions) → (40,batch_n*doc_l,hidden_dim*2)
-        # (h_n，c_n):保存着RNN最后一个时间步的隐藏层状态；保存着RNN最后一个时间步的细胞状态
-        # shape：(num_layers * num_directions, batch, hidden_size)
         sent_out, _ = self.sentLayer(documents, self.sent_hidden)  # sent_out: (sen_l, batch_n*doc_l, hidden_dim*2)
         # sent_out = self.dropout(sent_out)
 
         if mask is None:
-            # sentpres：(batch_n*doc_l,1,256)
-            # 激活函数
             sentpres = torch.tanh(torch.mean(sent_out, dim=0))  # sentpres: (batch_n*doc_l, hidden_dim*2)
         else:
             sent_out = sent_out.masked_fill(mask.transpose(1, 0).unsqueeze(-1).expand_as(sent_out), 0)
@@ -307,14 +180,9 @@ class EnSTWithRSbySPP(nn.Module):
         sentFt = self.sfLayer(sentpres)  # sentFt:(batch_n, doc_l,15)
         sentFt = self.dropout(sentFt)
 
-        # "add"情况下，将前三个pos位置1：1：1与sentence加和; ['gpos', 'lpos', 'ppos']
-        sentpres = self.posLayer(sentpres, pos)  # sentpres:(batch_n, doc_l, hidden_dim*2)
-        sentpres = sentpres.transpose(0, 1)  # sentpres: (doc_l, batch_n, hidden_dim*2)
+        sentpres = self.posLayer(sentpres, pos)   # sentpres:(batch_n, doc_l, hidden_dim*2)
+        sentpres = sentpres.transpose(0, 1)   # sentpres: (doc_l, batch_n, hidden_dim*2)
 
-        # LSTM(self.hidden_dim*2, self.sent_dim, bidirectional=True)
-        # input：(seq_len, batch, input_size)
-        # (h_0,c_0)：(num_layers * num_directions, batch, hidden_size)
-        # output: (seq_len, batch, sent_dim * num_directions)
         tag_out, _ = self.tagLayer(sentpres, self.tag_hidden)  # tag_out: (doc_l, batch_n, sent_dim*2)
         # tag_out = self.dropout(tag_out)
 
@@ -336,7 +204,7 @@ class EnSTWithRSbySPP(nn.Module):
 
     def getModelName(self):
         # pool_type='max_pool'的第一个字母m
-        name = 'st_rs_spp%s' % self.pool_type[0]
+        name = 'st_rs_gru_spp%s' % self.pool_type[0]
         name += '_' + str(self.hidden_dim) + '_' + str(self.sent_dim)
         if self.p_embd == 'cat':
             name += '_cp'
@@ -352,15 +220,11 @@ class EnSTWithRSbySPP(nn.Module):
 
 
 
-
-# type_id=2
-# 多了ft_size
-# sentence+额外的feature
-class STWithRSbySPPWithFt2(nn.Module):
+class STWithRSbySPPWithFt2_GRU(nn.Module):
     def __init__(self, word_dim, hidden_dim, sent_dim, class_n, p_embd=None, pos_dim=0, p_embd_dim=16, ft_size=0,
                  pool_type='max_pool'):
         # p_embd: 'cat', 'add','embd', 'embd_a'
-        super(STWithRSbySPPWithFt2, self).__init__()
+        super(STWithRSbySPPWithFt2_GRU, self).__init__()
         self.word_dim = word_dim
         self.hidden_dim = hidden_dim
         self.sent_dim = sent_dim
@@ -370,8 +234,9 @@ class STWithRSbySPPWithFt2(nn.Module):
         self.ft_size = ft_size
         self.pool_type = pool_type
 
-        self.dropout = nn.Dropout(0.5)
-        self.sentLayer = nn.LSTM(self.word_dim, self.hidden_dim, bidirectional=True)
+        self.dropout = nn.Dropout(0.1)
+        self.sentLayer = nn.GRU(self.word_dim, self.hidden_dim, bidirectional=True)
+
         self.classifier = nn.Linear(self.sent_dim * 2 + 30, self.class_n)
 
         self.posLayer = PositionLayer(p_embd, p_embd_dim)
@@ -379,17 +244,16 @@ class STWithRSbySPPWithFt2(nn.Module):
         self.rfLayer = InterSentenceSPPLayer(self.hidden_dim * 2, pool_type=self.pool_type)
 
         if p_embd == 'embd':
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2 + p_embd_dim * 3 + ft_size, self.sent_dim, bidirectional=True)
+            self.tagLayer = nn.GRU(self.hidden_dim * 2 + p_embd_dim * 3 + ft_size, self.sent_dim, bidirectional=True)
         elif p_embd == 'cat':
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2 + 3 + ft_size, self.sent_dim, bidirectional=True)
+            self.tagLayer = nn.GRU(self.hidden_dim * 2 + 3 + ft_size, self.sent_dim, bidirectional=True)
         else:
-            self.tagLayer = nn.LSTM(self.hidden_dim * 2 + ft_size, self.sent_dim, bidirectional=True)
+            self.tagLayer = nn.GRU(self.hidden_dim * 2 + ft_size, self.sent_dim, bidirectional=True)
 
     def init_hidden(self, batch_n, doc_l, device='cpu'):
-        self.sent_hidden = (torch.rand(2, batch_n * doc_l, self.hidden_dim, device=device).uniform_(-0.01, 0.01),
-                            torch.rand(2, batch_n * doc_l, self.hidden_dim, device=device).uniform_(-0.01, 0.01))
-        self.tag_hidden = (torch.rand(2, batch_n, self.sent_dim, device=device).uniform_(-0.01, 0.01),
-                           torch.rand(2, batch_n, self.sent_dim, device=device).uniform_(-0.01, 0.01))
+        # h0: [num_layers * num_directions, batch, hidden_size]
+        self.sent_hidden = torch.rand(2, batch_n * doc_l, self.hidden_dim, device=device).uniform_(-0.01, 0.01)
+        self.tag_hidden = torch.rand(2, batch_n, self.sent_dim, device=device).uniform_(-0.01, 0.01)
 
     # inputs:(30,25,40,768)
     # tp:(30,25,6)  前六个基础特征
@@ -441,7 +305,7 @@ class STWithRSbySPPWithFt2(nn.Module):
         return result
 
     def getModelName(self):
-        name = 'sent_SPP_%s_ft_%d' % (self.pool_type[0], self.ft_size)
+        name = 'sent_gru_%s_ft_%d' % (self.pool_type[0], self.ft_size)
         name += '_' + str(self.hidden_dim) + '_' + str(self.sent_dim) + '_' + str(self.ft_size)
         if self.p_embd == 'cat':
             name += '_cp'
