3aa0f4f687f4c609f1238ad29ba6b19e43f4ad81
whdcwsj <maxinaerichared@gmail.com>
2022-01-16 21:05:31 +0800

    1.16号的gat

--- gat1.sh
+++ gat1.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 1 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 100 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 200 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 300 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 400 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 500 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 600 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 700 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 800 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 900 --num_head 3 --model_name gat1_p3_lstm_w1_head3
-python chinese_train.py --gcn_conv_type 1 --model_type 3 --dgl_type lstm --weight_define 1 --dgl_layer 1 --seed_num 1000 --num_head 3 --model_name gat1_p3_lstm_w1_head3
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 1 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 100 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 200 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 300 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 400 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 500 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 600 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 700 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 800 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 900 --num_head 3 --model_name gat3_p3_w1_head3_loop
+python chinese_train.py --gcn_conv_type 1 --model_type 3 --weight_define 1 --dgl_layer 3 --add_self_loop 1 --seed_num 1000 --num_head 3 --model_name gat3_p3_w1_head3_loop
 
-python3 newtest.py --type_id 3 --model_name gat1_p3_lstm_w1_head3 --seed_start 0 --seed_end 11
+python newtest.py --type_id 3 --model_name gat3_p3_w1_head3_loop --seed_start 0 --seed_end 11
