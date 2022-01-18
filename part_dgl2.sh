b3d6715bf05294f021f645c1252de0bc760a5178
whdcwsj <maxinaerichared@gmail.com>
2022-01-18 23:14:19 +0800

    1.18号的part_dgl

--- part_dgl2.sh
+++ part_dgl2.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-python chinese_train.py --gcn_conv_type 0 --model_type 4 --dgl_type lstm --weight_define 3 --dgl_layer 2 --seed_num 200 --window_size 3 --model_name dgl2_p4_lstm_w3_size1
-python chinese_train.py --gcn_conv_type 0 --model_type 4 --dgl_type lstm --weight_define 3 --dgl_layer 2 --seed_num 300 --window_size 3 --model_name dgl2_p4_lstm_w3_size1
-python chinese_train.py --gcn_conv_type 0 --model_type 4 --dgl_type lstm --weight_define 3 --dgl_layer 2 --seed_num 400 --window_size 3 --model_name dgl2_p4_lstm_w3_size1
-python chinese_train.py --gcn_conv_type 0 --model_type 4 --dgl_type lstm --weight_define 3 --dgl_layer 2 --seed_num 500 --window_size 3 --model_name dgl2_p4_lstm_w3_size1
+python chinese_train.py --gcn_conv_type 0 --model_type 4 --dgl_type pool --add_self_loop 1 --weight_define 1 --dgl_layer 2 --seed_num 1 --window_size 1 --model_name dgl2_p4_pool_w1_size1_loop
+python chinese_train.py --gcn_conv_type 0 --model_type 4 --dgl_type pool --add_self_loop 1 --weight_define 1 --dgl_layer 2 --seed_num 100 --window_size 1 --model_name dgl2_p4_pool_w1_size1_loop
 
-python newtest.py --type_id 3 --model_name dgl2_p4_lstm_w3_size1 --seed_start 0 --seed_end 6
+python newtest.py --type_id 3 --model_name dgl2_p4_pool_w1_size1_loop --seed_start 0 --seed_end 2
+
+python chinese_train.py --gcn_conv_type 0 --model_type 4 --dgl_type pool --add_self_loop 1 --weight_define 3 --dgl_layer 2 --seed_num 1 --window_size 1 --model_name dgl2_p4_pool_w3_size1_loop
+python chinese_train.py --gcn_conv_type 0 --model_type 4 --dgl_type pool --add_self_loop 1 --weight_define 3 --dgl_layer 2 --seed_num 100 --window_size 1 --model_name dgl2_p4_pool_w3_size1_loop
+
+python newtest.py --type_id 3 --model_name dgl2_p4_pool_w3_size1_loop --seed_start 0 --seed_end 2
