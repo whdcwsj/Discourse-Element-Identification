0e81b5e7ade04b62c5f268e8ba3e84c17565ed0d
whdcwsj <maxinaerichared@gmail.com>
2022-04-19 21:50:58 +0800

    seed的样例

---
+++ seed_moban.py
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+seeds=(
+1
+100
+200
+300
+400
+500
+600
+700
+800
+900
+1000
+)
+
+for seed in ${seed[*]}
+do
+    python newtrain_en_ft.py  --dgl_type pool --weight_define 1 --dgl_layer 1 --window_size 2 --seed_num $seed --model_name dgl1_p4_pool_w1_size2
+done
+
+#!/bin/bash
+#
+# This script automatically runs
+#
+
+# EXPERIMENT_FOLDER=models/simple
+# EXPERIMENT_FILE=experiment-concat-embeddings.jsonnet
+# RESULTS_FOLDER=.
+#
+# #! Full path to application executable:
+# application="python -m allennlp.run evaluate"
+#
+# SEED=( 1 2 3 4 5 6 )
+#
+# for seed in "${SEED[@]}"
+# do
+#   EXPERIMENT_SPECIFIC_NAME=run-${seed}
+#   EXPERIMENT_SET_FOLDER=simple/final/${dataset}/use-ci-${use_ci}/${predictor_type}
+#   EXPERIMENT_NAME=${EXPERIMENT_SET_FOLDER}/${EXPERIMENT_SPECIFIC_NAME}
+#   CMD="$application ${RESULTS_FOLDER}/results/${EXPERIMENT_NAME}/model.tar.gz ../data/test/${TESTSET_FILENAME} --include-package models --output-file ${RESULTS_FOLDER}/results/${EXPERIMENT_NAME}/test-metrics.txt"
+#   echo $EXPERIMENT_NAME
+#   echo $CMD
+#   eval $CMD
+# done
